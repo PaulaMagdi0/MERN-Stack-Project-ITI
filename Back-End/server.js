9d4f33f2ed0ddc8db263e636922097a4cfd1f4eb
Paula Magdy <paulamagdy665@gmail.com>
2025-01-30 17:12:28 +0200

    Build the structure of backend

---
+++ Back-End/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Built-in body parser
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
