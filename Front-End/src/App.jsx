f4bd0550bc44515ed08aac0e2577b4f9e17e6f27
khaled-101 <khaled.shrief.dev@gmail.com>
2025-02-01 16:21:50 +0200

    Contact component

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
-
+import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/footer" element={<Footer />} />
+        <Route path="/Contact" element={<Contact/>}></Route>
       </Routes>
     </Router>
   );
