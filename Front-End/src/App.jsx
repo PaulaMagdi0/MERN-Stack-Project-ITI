b8a50fb258a7dbebf901919a7622984e70e24a4f
Paula Magdy <paulamagdy665@gmail.com>
2025-02-03 17:36:01 +0200

    404 Page

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
-import Contact from "./components/Contact/Contact";
+import Contact from "./components/contact/Contact";
 import MainPage from "./pages/mainPage/MainPage";
 import Navbar from "./components/nav/Navbar";
 import "./App.css";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<MainPage/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
--- Front-End/src/components/aboutas/About.jsx
+++ Front-End/src/components/aboutas/About.jsx
@@ -4,22 +4,21 @@ import book from "../../assets/73a24e15cbe0c7a17b5d9a82837c82ec.jpg"
 import background from "../../assets/965a493f279cfb106c2409a9d52dc473-removebg-preview.png"
 import watch from "../../assets/watch-removebg-preview.png";
 
-
 function AboutAs(params) {
     return ( 
         <>
             <search className="About">
 
-                <div class="content-container">
-                {/* <img src={watch} className="watch" /> */}
+                <div className="content-container">
+                {/* <img src={watch} classNameName="watch" /> */}
 
-                <h1 class="section-title">Welcome to Our Reading Community</h1>
+                <h1 className="section-title">Welcome to Our Reading Community</h1>
                 <br/>
-                <p class="section-text">
+                <p className="section-text">
                     Discover, share, and track your reading journey. Join our community of book lovers and explore hundreds of books.
                 </p>
                 
-                <p class="section-text">
+                <p className="section-text">
                     We are a fast-growing platform where you can find your next great read, rate books, write reviews, and connect with fellow readers. Whether you're into fiction, non-fiction, fantasy, or romance, there's a book for everyone.
                 </p>
                 </div>
