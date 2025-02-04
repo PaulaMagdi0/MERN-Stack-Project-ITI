e77c35d29f93b7fbf28026a7e7e34f5aee7d1142
“salmasaad2000” <salma.saad.2026@gmail.com>
2025-02-04 09:29:36 +0200

    payment backend

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Contact from "./components/Contact/Contact";
+import Payment from "./components/payment/payment";
 import MainPage from "./pages/mainPage/MainPage";
 import Navbar from "./components/nav/Navbar";
 import "./App.css";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage/>} />
         <Route path="/contact" element={<Contact/>} />
+        <Route path="/payment" element={<Payment/>} />
       </Routes>
       <Footer/>
     </BrowserRouter>
