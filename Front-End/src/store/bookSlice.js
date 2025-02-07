665436e0753ca26b38efc7677de6a104ba299890
“MahmoudNasrZ” <Mahmoudn.zalabany256@gmail.com>
2025-02-07 20:15:59 +0200

    Modification BackEndv1

--- Front-End/src/store/bookSlice.js
+++ Front-End/src/store/bookSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 // Async Thunks
 export const fetchBookById = createAsyncThunk(
